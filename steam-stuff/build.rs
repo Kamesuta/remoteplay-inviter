3062815c33fded868c05580236c141ed9742cae8
Kamesuta <kamesuta@gmail.com>
2024-09-02 02:02:16 +0900

    no bindgen

--- steam-stuff/build.rs
+++ steam-stuff/build.rs
@@ -1,21 +1,7 @@
 use cmake;
-use std::env;
-use std::path::PathBuf;
 
 fn main() {
     let dst = cmake::build("cmake");
     println!("cargo:rustc-link-search=native={}", dst.display());
     println!("cargo:rustc-link-lib=static=cmake");
-
-    let bindings = bindgen::Builder::default()
-        .header("cmake/src/Library.h")
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
-        .generate()
-        .expect("Unable to generate bindings");
-
-    // Write the bindings to the $OUT_DIR/bindings.rs file.
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    bindings
-        .write_to_file(out_path.join("bindings.rs"))
-        .expect("Couldn't write bindings!");
 }
--- steam-stuff/src/native.rs
+++ steam-stuff/src/native.rs
@@ -1,7 +1,22 @@
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
-#![allow(improper_ctypes)]
-#![allow(unused)]
+#[doc = "@brief Callback for when a Remote Play invite result is received.\n@param invitee The Steam ID of the invitee.\n@param guestID The guest ID of the invitee.\n@param connectURL The URL to connect to the Remote Play session."]
+pub type OnRemoteInvited = ::std::option::Option<
+    unsafe extern "C" fn(invitee: u64, guestID: u64, connectURL: *const ::std::os::raw::c_char),
+>;
 
-include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+#[doc = "@brief Callback for when a Remote Play session is started.\n@param invitee The Steam ID of the invitee.\n@param guestID The guest ID of the invitee."]
+pub type OnRemoteStarted = ::std::option::Option<unsafe extern "C" fn(invitee: u64, guestID: u64)>;
+
+#[doc = "@brief Callback for when a Remote Play session is closed.\n@param invitee The Steam ID of the invitee.\n@param guestID The guest ID of the invitee."]
+pub type OnRemoteStopped = ::std::option::Option<unsafe extern "C" fn(invitee: u64, guestID: u64)>;
+
+extern "C" {
+    pub fn SteamStuff_Init() -> bool;
+    pub fn SteamStuff_Shutdown();
+    pub fn SteamStuff_RunCallbacks();
+    pub fn SteamStuff_GetRunningGameID() -> u64;
+    pub fn SteamStuff_SendInvite(invitee: u64, gameID: u64) -> u64;
+    pub fn SteamStuff_CancelInvite(invitee: u64, guestID: u64);
+    pub fn SteamStuff_SetOnRemoteInvited(cb: OnRemoteInvited);
+    pub fn SteamStuff_SetOnRemoteStarted(cb: OnRemoteStarted);
+    pub fn SteamStuff_SetOnRemoteStopped(cb: OnRemoteStopped);
+}
